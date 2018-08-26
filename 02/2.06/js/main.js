419fb9fbf32d4ee0c7bb9ed22db30bacf6214409
Koster, Amanda EX1 <akoster@us.imshealth.com>
2018-08-26 16:43:32 -0700

    building my first bar chart

--- 02/2.06/js/main.js
+++ 02/2.06/js/main.js
@@ -5,14 +5,14 @@
 */
 //+d.age makes numbners as strings into numbers as i
 // var data = [25, 20, 10, 12, 15];
-d3.json("data/ages.json").then(function (data) {
-    data.forEach(function (d) {
+d3.json("data/ages.json").then(data =>  {
+    data.forEach(d => {
         d.age = +d.age;
     });
 
     var svg = d3.select("#chart-area").append("svg")
-        .attr("width", 400)
-        .attr("height", 400);
+        .attr("width", 600)
+        .attr("height", 600);
 
     //sort of like mapping though data
     var circles = svg.selectAll("circle")
@@ -22,10 +22,10 @@ d3.json("data/ages.json").then(function (data) {
     circles.enter()
         .append("circle")
         .attr("cx", (d, i) => (i * 100) + 25) //stagger distance from left
-        .attr("cy", 50) //distance from top
+        .attr("cy", 100) //distance from top
         .attr("r", (d) => d.age * 2) //radius apart
         .attr("fill", d => {
-            if (d.name === "Tony")
+            if (d.name === "Emily")
                 return "blue"
             else return "red"
 
--- 02/2.08.0/js/main.js
+++ 02/2.08.0/js/main.js
@@ -3,3 +3,17 @@
 *    Mastering Data Visualization with D3.js
 *    2.8 - Activity: Your first visualization!
 */
+
+d3.json("data/buildings.json").then(data => {
+  data.forEach( d => {  
+    d.height = +d.height;
+  });
+
+  var svg = d3.select("chart-area").append("svg")
+    .attr("width", 700)
+    .attr("height", 700)
+
+    var rectangles = svg.selectAll("rectangle")
+    .data(data)
+    console.log(data, "d.height")
+});
