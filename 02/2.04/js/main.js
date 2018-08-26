2567a54a42b68f450533ccc65075b196fc2cac5e
Koster, Amanda EX1 <akoster@us.imshealth.com>
2018-08-26 15:14:53 -0700

    drew a square

--- 02/2.04/js/main.js
+++ 02/2.04/js/main.js
@@ -5,11 +5,18 @@
 */
 
 var svg = d3.select("#chart-area").append("svg")
-	.attr("width", 400)
-	.attr("height", 400);
+	.attr("width", 1000)
+	.attr("height", 1500);
 
 var circle = svg.append("circle")
 	.attr("cx", 100)
 	.attr("cy", 250)
 	.attr("r", 70)
-	.attr("fill", "grey");
+	.attr("fill", "red");
+
+var rect = svg.append("rect")
+	.attr("x", 20)
+	.attr("y", 20)
+	.attr("width", 100)
+	.attr("height", 100)
+	.attr("fill", "pink")
