f325d9bc983864947372bd8bdbb8c561d75323c1
Koster, Amanda EX1 <akoster@us.imshealth.com>
2018-08-26 17:06:13 -0700

    chart on page - vanilla js remember to add # for id

--- 02/2.08.0/js/main.js
+++ 02/2.08.0/js/main.js
@@ -3,17 +3,24 @@
 *    Mastering Data Visualization with D3.js
 *    2.8 - Activity: Your first visualization!
 */
+var svg = d3.select("#chart-area")
+  .append("svg")
+  .attr("width", "400")
+  .attr("height", "400")
 
 d3.json("data/buildings.json").then(data => {
-  data.forEach( d => {  
+  console.log(data)
+  data.forEach(d => {
     d.height = +d.height;
   });
 
-  var svg = d3.select("chart-area").append("svg")
-    .attr("width", 700)
-    .attr("height", 700)
-
-    var rectangles = svg.selectAll("rectangle")
+  var rects = svg.selectAll("rect")
     .data(data)
-    console.log(data, "d.height")
+    .enter()
+    .append("rect")
+    .attr("y", 0)
+    .attr("x", (d, i) => i * 60)
+    .attr("width", 40)
+    .attr("height", d => d.height)
+    .attr("fill", d => "pink")
 });
--- 02/2.08.1/js/main.js
+++ 02/2.08.1/js/main.js
@@ -9,26 +9,26 @@ var svg = d3.select("#chart-area")
     .attr("width", "400")
     .attr("height", "400");
 
-d3.json("data/buildings.json").then(function(data){
+d3.json("data/buildings.json").then(function (data) {
     console.log(data);
 
-    data.forEach(function(d) {
+    data.forEach(function (d) {
         d.height = +d.height;
     });
 
     var rects = svg.selectAll("rect")
-            .data(data)
+        .data(data)
         .enter().append("rect")
-            .attr("y", 0)
-            .attr("x", function(d, i){
-                return (i * 60);
-            })
-            .attr("width", 40)
-            .attr("height", function(d){
-                return d.height;
-            })
-            .attr("fill", function(d) {
-                return "grey";
-            });
+        .attr("y", 0)
+        .attr("x", function (d, i) {
+            return (i * 60);
+        })
+        .attr("width", 40)
+        .attr("height", function (d) {
+            return d.height;
+        })
+        .attr("fill", function (d) {
+            return "grey";
+        });
 
 })
