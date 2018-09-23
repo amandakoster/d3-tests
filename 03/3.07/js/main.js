7de07688b1220c8e257d3dc5e49ad4e518be9152
Koster, Amanda EX1 <akoster@us.imshealth.com>
2018-09-23 16:06:51 -0700

    flipped the values to read correctly

--- 03/3.07/js/main.js
+++ 03/3.07/js/main.js
@@ -7,36 +7,36 @@
 var margin = { left: 100, right: 10, top: 10, bottom: 150 };
 
 var width = 600 - margin.left - margin.right;
-    height = 400 - margin.top - margin.bottom;
+height = 400 - margin.top - margin.bottom;
 
 // CHART AREA
-var g = d3.select("#chart-area")
+var g = d3
+  .select("#chart-area")
   .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
   .append("g")
-    .attr("transform", "translate(" + margin.left 
-        + ", " + margin.top + ")"); 
+  .attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
 
 // X Label
 g.append("text")
-    .attr("class", "x axis-label")
-    .attr("x", width / 2)
-    .attr("y", height + 140)
-    .attr("font-size", "20px")
-    .attr("text-anchor", "middle")
-    .text("The word's tallest buildings");
+  .attr("class", "x axis-label")
+  .attr("x", width / 2)
+  .attr("y", height + 140)
+  .attr("font-size", "20px")
+  .attr("text-anchor", "middle")
+  .text("The word's tallest buildings");
 
 // Y Label
 g.append("text")
-    .attr("class", "y axis-label")
-    .attr("x", - (height / 2))
-    .attr("y", -60)
-    .attr("font-size", "20px")
-    .attr("text-anchor", "middle")
-    .attr("transform", "rotate(-90)")
-    .text("Height (m)");
-    
+  .attr("class", "y axis-label")
+  .attr("x", -(height / 2))
+  .attr("y", -60)
+  .attr("font-size", "20px")
+  .attr("text-anchor", "middle")
+  .attr("transform", "rotate(-90)")
+  .text("Height (m)");
+
 // DATA + CHART FUNCTION
 d3.json("data/buildings.json").then(function(data) {
   console.log(data);
@@ -45,45 +45,51 @@ d3.json("data/buildings.json").then(function(data) {
     d.height = +d.height;
   });
 
-  // BANDS for X and Y 
+  // BANDS for X and Y
   var x = d3
     .scaleBand()
-    .domain(
-      data.map((d) => d.name))
+    .domain(data.map(d => d.name))
     .range([0, width])
     .paddingInner(0.3)
     .paddingOuter(0.3);
 
-var y = d3
+  var y = d3
     .scaleLinear()
     .domain([0, d3.max(data, d => d.height)])
-    .range([0, height]);
+    .range([height, 0]);
 
-var xAxisCall = d3.axisBottom(x);
-g.append("g")
+  var xAxisCall = d3.axisBottom(x);
+  g.append("g")
     .attr("class", "x axis")
     .attr("transform", "translate(0, " + height + ")")
-  .call(xAxisCall)
+    .call(xAxisCall)
     .selectAll("text")
-    .attr("y", "10")
-    .attr("x", "-5")
-    .attr("text-anchor", "end")
-    .attr("transform", "rotate(-40)");
+      .attr("y", "10")
+      .attr("x", "-5")
+      .attr("text-anchor", "end")
+      //transform x axis labels so they do not overlap
+      .attr("transform", "rotate(-40)");
 
-var yAxisCall = d3.axisLeft(y)
-  .ticks(3)
-  .tickFormat(d => d + "m");
-g.append("g")
-  .attr("class", "y-axis")
-  .call(yAxisCall);
+  //Ticks
+  var yAxisCall = d3
+    .axisLeft(y)
+    // 3 ticks
+    .ticks(3)
+    //ticks have an 'm' for meters
+    .tickFormat(d => d + "m");
+  g.append("g")
+    .attr("class", "y-axis")
+    .call(yAxisCall);
 
   var rects = g.selectAll("rect").data(data);
 
-  rects.enter()
+  rects
+    .enter()
     .append("rect")
-    .attr("y", 0)
-    .attr("x", (d) => x(d.name))
+
+    .attr("y", d => y(d.height))
+    .attr("x", d => x(d.name))
     .attr("width", x.bandwidth)
-    .attr("height", (d) => y(d.height))
+    .attr("height", d => height - y(d.height))
     .attr("fill", "pink");
 });
